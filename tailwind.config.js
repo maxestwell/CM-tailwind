56e56c329419777dd279f856a40e73b9488c4c0d
maxestwell <maxwellpcorcoran@gmail.com>
2024-03-19 13:22:34 +1100

    import from adobe fonts to html header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: [
-		'./templates/*.html',
-		'./templates/*.twig',
-		'./templates/partial/*.twig',
-		'./templates/blocks/*.twig',
-		// Add paths to your Twig templates or any files using Tailwind classes
-	],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#3490dc', // Add your custom primary color
-				secondary: '#6574cd', // Add your custom secondary color
-				debug: '#FFCA28', // Just so you can see how this is powerful
-				// Add more custom colors as needed
-			},
-			fontFamily: {
-				//font files are declared in html-header.twig file
-				'sans': ['"Inria Sans"', 'sans-serif'],
-			},
-		}
-	},
-	plugins: [],
-}
-
+  content: [
+    "./templates/*.html",
+    "./templates/*.twig",
+    "./templates/partial/*.twig",
+    "./templates/blocks/*.twig",
+    // Add paths to your Twig templates or any files using Tailwind classes
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3490dc", // Add your custom primary color
+        secondary: "#6574cd", // Add your custom secondary color
+        debug: "#FFCA28", // Just so you can see how this is powerful
+        // Add more custom colors as needed
+      },
+      fontFamily: {
+        //font files are declared in html-header.twig file
+        sans: ['"Inria Sans"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
